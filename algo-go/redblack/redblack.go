1ffb72603c20218c2e151c49c409e45c0f0bdb08
fractalbach <fractalbach@gmail.com>
2022-08-05 05:43:53 -0700

    added insert

--- algo-go/redblack/redblack.go
+++ algo-go/redblack/redblack.go
@@ -1,13 +1,15 @@
 package redblack
 
+import "golang.org/x/exp/constraints"
+
 // Tree is a Red-Black Tree. Create one by using NewTree[T comparable](value T)
 // to initialize the root of the tree.
-type Tree[T comparable] struct {
+type Tree[T constraints.Ordered] struct {
 	root *node[T]
 }
 
 // node in a red-black tree
-type node[T comparable] struct {
+type node[T constraints.Ordered] struct {
 	parent *node[T]
 	left   *node[T]
 	right  *node[T]
@@ -25,7 +27,7 @@ const (
 // NewTree creates and initializes the root of a new Red-Black Tree. This will
 // also determine the type of values that the tree contains. Since Red-Black
 // trees are a type of binary search tree, the values must be comparable.
-func NewTree[T comparable](value T) *Tree[T] {
+func NewTree[T constraints.Ordered](value T) *Tree[T] {
 	return &Tree[T]{
 		root: &node[T]{nil, nil, nil, black, value},
 	}
@@ -74,4 +76,32 @@ func (t *Tree[T]) rightRotate(y *node[T]) {
 
 	x.right = y // y becomes x's right subtree
 	y.parent = x
+}
+
+// Insert an element into the red-black tree in O(lg n) time complexity. The
+// tree will automatically balance itself to ensure this.
+func (t *Tree[T]) Insert(value T) {
+	var x *node[T] = t.root                 // 'current' node as we traverse
+	var y *node[T] = nil                    // parent of node to insert
+	var z *node[T] = &node[T]{value: value} // node to insert
+	for x != nil {
+		y = x
+		if z.value < y.value {
+			x = x.left
+		} else {
+			x = x.right
+		}
+	}
+	z.parent = y
+	if y == nil {
+		t.root = z
+	}
+	z.left = nil
+	z.right = nil
+	z.color = red
+	t.insertFixup()
+}
+
+func (t *Tree[T]) insertFixup() {
+
 }
