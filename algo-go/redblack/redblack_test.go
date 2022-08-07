9ebf3303b392a3016eb5e9aa0c1478a7c22a7759
fractalbach <fractalbach@gmail.com>
2022-08-07 00:05:51 -0700

    added a test for rotations

--- algo-go/redblack/redblack_test.go
+++ algo-go/redblack/redblack_test.go
@@ -12,3 +12,17 @@ func TestHello(t *testing.T) {
 	t.Logf("tree: %v\n", tree)
 	t.Logf("%v", tree.string())
 }
+
+func TestRotation(t *testing.T) {
+	tree := NewTree(5)
+	for _, v := range []int{2, 1, 3, 7} {
+		tree.Insert(v)
+	}
+	t.Logf("tree starts as: %v", tree.string())
+	tree.rightRotate(tree.root)
+	t.Logf("do rightRotate: %v", tree.string())
+	tree.leftRotate(tree.root)
+	t.Logf("do leftRotate : %v", tree.string())
+}
+
+// (((1) 2 (3)) 5 (7))
